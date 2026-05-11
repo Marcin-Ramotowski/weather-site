4dca94250df0739e58b3287cab6ff25f4e1fb46b
Marcin-Ramotowski <marcin00.mr@gmail.com>
2026-05-11 22:18:13 +0200

    Updated url to Europe baric map

--- files/baric.php
+++ files/baric.php
@@ -1,5 +1,5 @@
 <?php $title = "Mapa baryczna"; include("template.php"); ?>
     <h1>Mapa baryczna Europy</h1>
-    <img class="data" src="https://wind.met.fu-berlin.de/loops/DWD_Karten/gme_tkb_na_p_036_000.gif"
+    <img class="data" src="https://wind.met.fu-berlin.de/storage/wetterkarten/gme_tkb_na_p_036_000.gif"
         alt="Aktualna sytuacja baryczna w Europie">
 <?php include("footer.php"); ?>
